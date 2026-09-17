3257de90097d50f3384b69b2605fd7ab54aa3651
danimoncerro <dani_moncerro@yahoo.com>
2026-09-17 18:51:21 +0300

    Import Categories

--- app/controllers/ImportController.php
+++ app/controllers/ImportController.php
@@ -17,9 +17,33 @@ class ImportController
         $destFile = $uploadDir.$file['name'];
 
         if(move_uploaded_file($sourceFile, $destFile)){
-            echo 'Csv uploaded';
+            //echo 'Csv uploaded';
+
+            $handle = fopen($destFile, 'r');
+            fgetcsv($handle);
+
+            $categoryModel = new Category();
+            while (($row = fgetcsv($handle)) !== false) {
+                if (!isset($row[0])) {
+                    continue;
+                }
+
+                $categoryName = $row[0];
+                echo "$categoryName <br>";
+                $categoryModel->create(
+                    $categoryName
+                );
+
+            }
+
+            fclose($handle);
+            
+
+
         } else {
             echo 'Csv did not upload';
         }
     }
 }
+
+
